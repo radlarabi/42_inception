7be72a77aad52a6366ef203b093b837c7b78769e
radlarabi <rlarabi@student.1337.ma>
2024-10-20 23:06:44 +0100

    add normal user in wordpress && get domain name in nginx from env

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,7 +1,8 @@
 mkdir /etc/nginx/ssl
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_PRIV -out $CERTS_ -subj "/C=MA/ST=IDF/L=Benguerir/O=42/OU=42/CN=rlarabi.42.fr/UID=rlarabi"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_PRIV -out $CERTS_ -subj "/C=MA/ST=IDF/L=Benguerir/O=42/OU=42/CN=$DOMAIN_NAME/UID=rlarabi"
 
+sed -i "s|server_name PATH_OF_DOMAIN_NAME;|server_name $DOMAIN_NAME;|" /etc/nginx/sites-enabled/nginx.conf
 sed -i "s|ssl_certificate PATH_OF_CERTS_;|ssl_certificate $CERTS_;|" /etc/nginx/sites-enabled/nginx.conf
 sed -i "s|ssl_certificate_key PATH_OF_CERTS_PRIV;|ssl_certificate_key $CERTS_PRIV;|" /etc/nginx/sites-enabled/nginx.conf
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sleep 2
 
 sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '$MYSQL_DATABASE' );/" /var/www/wordpress/wp-config.php;
 sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER' );/" /var/www/wordpress/wp-config.php;
@@ -10,5 +11,6 @@ chmod +x wp-cli.phar;
 mv wp-cli.phar /usr/local/bin/wp
 
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path="/var/www/wordpress" --allow-root;
-
+wp user create $WP_USER_USERNAME $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --path="/var/www/wordpress" --allow-root;
+echo "user create $WP_USER_USERNAME $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --path="/var/www/wordpress" --allow-root"
 php-fpm8.2 -F;
