df41f6d1e0a302364e57081570fd398eb136b961
radlarabi <rlarabi@student.1337.ma>
2024-10-20 15:00:09 +0100

    get the path of certificate form env

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,14 +1,8 @@
-# mkdir /etc/nginx/ssl && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_PRIV -out $CERTS_ -subj "/C=MA/ST=IDF/L=Benguerir/O=42/OU=42/CN=rlarabi.42.fr/UID=rlarabi"
+mkdir /etc/nginx/ssl
 
-# sleep 1
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_PRIV -out $CERTS_ -subj "/C=MA/ST=IDF/L=Benguerir/O=42/OU=42/CN=rlarabi.42.fr/UID=rlarabi"
 
-# sed -e "s/ssl_certificate \/etc\/nginx\/ssl\/fullchain.pem;/ssl_certificate $CERTS_;/" /etc/nginx/sites-enabled/nginx.conf
-# sed -e "s/ssl_certificate_key \/etc\/nginx\/ssl\/privkey.pem;/ssl_certificate_key $CERTS_PRIV;/" /etc/nginx/sites-enabled/nginx.conf
+sed -i "s|ssl_certificate PATH_OF_CERTS_;|ssl_certificate $CERTS_;|" /etc/nginx/sites-enabled/nginx.conf
+sed -i "s|ssl_certificate_key PATH_OF_CERTS_PRIV;|ssl_certificate_key $CERTS_PRIV;|" /etc/nginx/sites-enabled/nginx.conf
 
-
-# mkdir /etc/nginx/ssl && openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_PRIV -out /etc/nginx/ssl/fullchain.pem -subj "/C=MA/ST=IDF/L=Benguerir/O=42/OU=42/CN=rlarabi.42.fr/UID=rlarabi"
-
-# sleep 1
-
-# sed -e "s/ssl_certificate \/etc\/nginx\/ssl\/fullchain.pem;/ssl_certificate $CERTS_;/" /etc/nginx/sites-enabled/nginx.conf
-# sed -e "s/ssl_certificate_key \/etc\/nginx\/ssl\/privkey.pem;/ssl_certificate_key $CERTS_PRIV;/" /etc/nginx/sites-enabled/nginx.conf
+nginx -g "daemon off;"
